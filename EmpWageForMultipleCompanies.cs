7ce428978cab90d2bcca8c9c263171bcfdcf77bf
1SurajDesai <surajdesai219@gmail.com>
2022-03-02 11:59:39 +0530

    Adding code to calculate employee wage for multiple companies

---
+++ EmpWageForMultipleCompanies.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageProblemUC7AndUC8
+{
+    public class EmpWageForMultipleCompanies
+    {
+        public const int IsPartTime = 1;
+        public const int IsFullTime = 2;
+        
+        public void ComputeEmpWage(string Company, int EmpRatePerHr, int NoOfWorkingDays, int MaxRateInMonth)
+        {
+            int EmpHr = 0, TotalEmpHr = 0, TotalWorkingDays = 0;
+            while (TotalEmpHr <= MaxRateInMonth && TotalWorkingDays < NoOfWorkingDays)
+            {
+                TotalWorkingDays++;
+                Random random = new Random();
+                int EmpCheck = random.Next(3);
+                switch (EmpCheck)
+                {
+                    case IsPartTime:
+                        EmpHr = 4;
+                        break;
+                    case IsFullTime:
+                        EmpHr = 8;
+                        break;
+                    default:
+                        EmpHr = 0;
+                        break;
+                }
+                TotalEmpHr += EmpHr;
+
+            }
+            int salary = TotalEmpHr * EmpRatePerHr; 
+            Console.WriteLine("Total salary for company " + Company + " is " + salary);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,10 +6,10 @@ namespace EmpWageProblemUC7AndUC8
         static void Main(string[] args)
         {
             //Console.WriteLine("Welcome to Employee Wage Computattion Problem");
-            RefactorCodeToComputeWage obj1 = new RefactorCodeToComputeWage();   
-            obj1.ComputeEmpWage();
-            //EmpWageForMultipleCompanies obj2 = new EmpWageForMultipleCompanies();
-            //obj2.ComputeEmpWage("Reliance", 20, 30, 100);
+            //RefactorCodeToComputeWage obj1 = new RefactorCodeToComputeWage();   
+            //obj1.ComputeEmpWage();
+            EmpWageForMultipleCompanies obj2 = new EmpWageForMultipleCompanies();
+            obj2.ComputeEmpWage("Reliance", 20, 30, 100);
 
 
         }
