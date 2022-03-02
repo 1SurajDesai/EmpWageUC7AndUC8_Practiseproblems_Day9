1ad54bc2ed0bc7607a0e3c9a5e4ddf4fa33f86b6
1SurajDesai <surajdesai219@gmail.com>
2022-03-02 11:53:50 +0530

    Adding Program.cs file

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace EmpWageProblemUC7AndUC8
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computattion Problem");
+            //RefactorCodeToComputeWage obj1 = new RefactorCodeToComputeWage();   
+            //obj1.ComputeEmpWage();
+            //EmpWageForMultipleCompanies obj2 = new EmpWageForMultipleCompanies();
+            //obj2.ComputeEmpWage("Reliance", 20, 30, 100);
+
+
+        }
+    }
+
+
+}
